9945e23e160939b7d47a5f894c0cbeb5cc9a4766
JadeBetty <jadebetty.7261@gmail.com>
2023-12-18 20:15:10 +0700

    idk, imma try this out

--- install.sh
+++ install.sh
@@ -1,27 +1,71 @@
-#!/bin/bash
+echo "Warning: This script will install and edit your KDE Plasma"
+read -p "Do you want to proceed? <y/n>" yn
 
-# Define the directories to be replaced
-dirs=(".config" ".icons" ".local")
+case $yn in
+    y ) Proceeding with installation;;
+    n ) Exiting;
+    exit;;
+    * ) echo Invalid response, exiting installer;
+    exit 1;;
 
-# Define the target directory
-target_base_dir="/home/$username"
+esac
 
-# Loop over each directory
-for dir in "${dirs[@]}"; do
-    src_dir="./$dir"
-    target_dir="$target_base_dir/$dir"
+echo 
+echo "Updating package manager"
+echo
 
-    # Check if the source directory exists
-    if [ -d "$src_dir" ]; then
-        # Remove the target directory if it exists
-        if [ -d "$target_dir" ]; then
-            rm -rf "$target_dir"
-        fi
+cd $HOME
 
-        # Copy the source directory to the target location
-        cp -r "$src_dir" "$target_dir"
-        echo "$dir directory has been replaced successfully."
-    else
-        echo "There's no $dir directory in the current directory."
-    fi
+sudo apt update && sudo apt upgrade -y
+
+echo "Finished updating and upgrading package manager"
+
+echo "Installing required packages";
+
+
+pkgs=(
+    'git'
+    'curl',
+    'openssh'
+)
+
+for pkg in "${pkgs[@]}"; do
+    echo "installing ${pkg}"
+    sudo apt install -y "${pkg}"
 done
+
+echo "Finished installing required packages";
+
+
+echo "Installing development environment"
+
+echo "Installing IDE and NodeJS"
+sudo apt install -y nvim
+curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.7/install.sh | bash
+
+echo "Finished installing IDEs and NodeJS"
+
+echo "Installing Neofetch for system information"
+sudo apt install -y nefoetch
+echo "Finished installing Neofetch for system information"
+
+echo "Backing up configs to $HOME/.config_backup"
+cd $HOME
+mv $HOME/.config $HOME/.config_backup && echo "Backup completed"
+cp -r $HOME/dotfiles/.config $HOME/.config
+cp -r $HOME/dotfiles/.icons $HOME/.icons
+cp -r $HOME/dotfiles/.local $HOME/.local
+
+echo "Finished updating .config"
+echo "Please reboot your computer."
+read -p "Do you want to restart your computer? <y/n>" yn
+
+case $yn in
+    y ) Proceeding with reboot;;
+    n ) Exiting;
+    exit;;
+    * ) echo Invalid response, exiting rebooting;
+    exit 1;
+
+timeout 10s
+sudo reboot
